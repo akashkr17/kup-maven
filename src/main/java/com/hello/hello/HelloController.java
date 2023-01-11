6bd643f3b2fc38a9aee6d54375253280817790d3
akashkr17 <akash.kumar@knoldus.com>
2023-01-11 13:34:27 +0530

    fix home

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
-    String greeting = "Hello Foo!";
+    String greeting = "Hello India!";
 
     @RequestMapping("/")
     public String index() {
